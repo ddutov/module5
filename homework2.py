124165c3b5a9b031c6c5d19dc196dea2229a47e9
ddutov <165052103+ddutov@users.noreply.github.com>
2024-05-13 22:00:37 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -2,7 +2,7 @@ class House:
     def __init__(self):
         self.number_of_floors = 0
 
-    def set_new_number_of_floors(self, floors):
+    def setNewNumberOfFloors(self, floors):
         """ Установить новое количество этажей """
         self.number_of_floors = floors
         print('Этажность установлена. В Cтроении ', self.number_of_floors, 'этажей!')
@@ -10,4 +10,4 @@ class House:
 
 build_1 = House()
 print('Объект "Строение" создан. Количество этажей: ', build_1.number_of_floors)
-build_1.set_new_number_of_floors(input('Задайте требуемое количество этажей: '))
+build_1.setNewNumberOfFloors(input('Задайте требуемое количество этажей: '))
