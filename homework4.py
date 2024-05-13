de38fffb1c1ba5bf11403c17c7b1fe25cdadd2d6
ddutov <165052103+ddutov@users.noreply.github.com>
2024-05-13 23:06:50 +0300

    homework4.py

--- homework4.py
+++ homework4.py
@@ -1,10 +1,15 @@
 class Buiding:
     total = 0
+
     def __init__(self):
-       Buiding.total += 1
+        Buiding.total += 1
+
+    def __str__(self):
+        return f'новый объект № {self.total}'
 
 
 for i in range(0, 40):
     new_object = Buiding()
-    print('новый объект № ', Buiding.total)
+    # print('новый объект № ', Buiding.total)
+    print(new_object)
     i += 1
